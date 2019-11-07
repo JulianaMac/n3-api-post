c5f84cf9a479268c8dcdc4cf27ba43d522afbbec
JulianaMac <juliana.s.machado@hotmail.com>
2019-11-06 22:25:51 -0300

    first commit

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -1,18 +1,34 @@
-const contatos = require("../model/contatos");
+const model = require("../model/contatos");
 
 const getAll = (request, response) => {
   console.log(request.url);
-  response.status(200).send(contatos.model);
+  response.status(200).send(model.agenda);
 };
 
-const getById = (request, response) => {
-  const id = request.params.id;
-  response.status(200).send(contatos.find(tarefa => tarefa.id == id));
+const add = (request, response) => {
+
+  let contato = request.body
+  let verificaContato = model.agenda.contatos
+  
+  verificaContato.forEach(novoContato => {
+
+  if (novoContato.nome !== contato.nome){
+
+  contato.id = Math.random().toString(36).substr(-8)
+
+  model.agenda.contatos.push(contato);
+  response.status(200).send(request.body)
+
+  } else {
+ console.log("Já existe esse nome")
+  response.status(400).send(request.body)
+  }
+  })
 };
 
 
 module.exports = {
   getAll,
-  getById
+  add
 }
 
