2b12b44fa0df76ffbe7eee93ceda3ce2a7dc11c3
JulianaMac <juliana.s.machado@hotmail.com>
2019-11-07 22:06:20 -0300

    comparar contatos

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -5,27 +5,34 @@ const getAll = (request, response) => {
   response.status(200).send(model.agenda);
 };
 
+// const add = (request, response) => {
+
+//   let contato = request.body
+//   let verificaContato = model.agenda.contatos
+  
+
+// }
+
 const add = (request, response) => {
 
   let contato = request.body
-  let verificaContato = model.agenda.contatos
-  
-  verificaContato.forEach(novoContato => {
-
-  if (novoContato.nome !== contato.nome){
+  let baseDados = model.agenda.contatos
 
   contato.id = Math.random().toString(36).substr(-8)
+  
 
-  model.agenda.contatos.push(contato);
-  response.status(200).send(request.body)
-
+  if (!contato.nome || !contato.dataNascimento || !contato.celular) {
+    response.status(400).send("Dados inválidos");
   } else {
- console.log("Já existe esse nome")
-  response.status(400).send(request.body)
+    if (baseDados.find(dado => dado.nome === contato.nome)) {
+      response.status(400).send("Contato já cadastrado")
+    } else {
+      model.agenda.contatos.push(contato)
+      response.status(201).send(contato)
+    }
   }
-  })
-};
 
+  }
 
 module.exports = {
   getAll,
--- servidor-contato/src/routes/contatosRoute.js
+++ servidor-contato/src/routes/contatosRoute.js
@@ -4,6 +4,6 @@ const bodyParser = require('body-parser')
 const controller = require("../controller/contatosController")
 
 router.get("/", controller.getAll)
-router.post("/criar", bodyParser.json(), controller.add)
+router.post("/criar/comparar", bodyParser.json(), controller.add)
 
 module.exports = router
